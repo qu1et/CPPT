ca54593500d5ee694fb793ae59f63fef41073c00
qu1et <47870708+qu1et@users.noreply.github.com>
2019-05-31 17:04:38 +0300

    Add files via upload

---
+++ lab10/myArt/blog/static/fold-post.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let foldBtns = document.getElementsByClassName("btn"),
+    cont = document.getElementsByClassName('one-post');
+
+console.log(cont);
+
+for (let i = 0; i < foldBtns.length; i++) {
+    foldBtns[i].addEventListener("click", function(e) {
+        if (e.target.parentElement.className == "one-post folded") {
+            e.target.innerHTML = "свернуть";
+            e.target.parentElement.className = "one-post";
+        }
+        else {
+            e.target.parentElement.className = "one-post folded";
+            e.target.innerHTML = "развернуть";
+        }
+        console.log("you clicked", event.target);
+    });
+
+}
---
+++ lab10/myArt/blog/static/highlight-post.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('.one-post').hover(function(event) {
+        console.log('Навели');
+        console.log(event.currentTarget);
+    }, function(event) {
+        console.log('Вывели')
+    });
+});
+
+$(document).ready(function() {
+    $('.one-post').hover(function(event) {
+        $(event.currentTarget).find('.one-post-shadow').animate({opacity: '0.1'}, 300);
+    }, function(event) {
+        $(event.currentTarget).find('.one-post-shadow').animate({opacity: '0'}, 300);
+    })
+});
+
+$(document).ready(function() {
+    $('.logo-img').hover(function(event) {
+        $(event.currentTarget).find('.logo').animate({width: '338px', height: 'auto'}, 300);
+    }, function(event) {
+        $(event.currentTarget).find('.logo').animate({width: '318px', height: 'auto'}, 300);
+    })
+});
